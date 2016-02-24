cb888225cf7ba99b423eb487e41277e9bc456089
varun kumar <varun.kumar@tothenew.com>
2016-02-24 15:34:43 +0530

    Initial Commit

---
+++ PerApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PerApp
+//
+//  Created by Varun Kumar on 24/02/16.
+//  Copyright © 2016 Varun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
